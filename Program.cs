300b9fe5b10e64d7e913a62f9b29bbf83541a37b
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-04-24 15:29:54 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,80 +1,39 @@
 ﻿using System;
 
-namespace ข้อสอบข้อ3
+namespace ข้อสอบ4_DDA
 {
     class Program
     {
         static void Main(string[] args)
         {
+            float x, y, dx, dy, step;
+            float x1, y1, x2, y2;
 
-            int box1, box2, box3;
-            int numbucket, pickbox;
-            bool gameStart = true;
-            Console.Write("input box1 in the bucket : ");
-            box1 = int.Parse(Console.ReadLine());
-            Console.Write("input box2 in the bucket : ");
-            box2 = int.Parse(Console.ReadLine());
-            Console.Write("input box3 in the bucket : ");
-            box3 = int.Parse(Console.ReadLine());
+            x1 = float.Parse(Console.ReadLine());
+            y1 = float.Parse(Console.ReadLine());
+            x2 = float.Parse(Console.ReadLine());
+            y2 = float.Parse(Console.ReadLine());
 
-            while (gameStart == true)
+            dx = x2 - x1;
+            dy = y2 - y1;
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            { step = Math.Abs(dx); }
+            else
+            { step = Math.Abs(dy); }
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+            int i = 1;
+            while (i <= step)
             {
-                Console.WriteLine("YOU A");
-                Console.WriteLine("Box1 : {0},Box2 : {1},Box 3 : {2}",box1,box2,box3);
-                Console.Write("Choose number bucket : ");
-                numbucket = int.Parse(Console.ReadLine());
-                Console.Write("How much do you want to pick : ");
-                pickbox = int.Parse(Console.ReadLine());
-
-                switch (numbucket)
-                {
-                    case 1:
-                        box1 = box1 - pickbox;
-                        break;
-                    case 2:
-                        box2 = box2 - pickbox;
-                        break;
-                    case 3:
-                        box3 = box3 - pickbox;
-                        break;
-                    default:
-                        Console.WriteLine("Not found");
-                        break;
-                }
-                if (box1 == 0 & box2 == 0 & box3 == 0)
-                {
-                    Console.WriteLine("The Winner is B");
-                    gameStart = false;       
-                }
-                Console.WriteLine("YOU B");
-                Console.WriteLine("Box1 : {0},Box2 : {1},Box 3 : {2}", box1, box2, box3);
-                Console.Write("Choose number bucket : "); 
-                numbucket = int.Parse(Console.ReadLine());
-                Console.Write("How much do you want to pick : ");
-                pickbox = int.Parse(Console.ReadLine());
-
-                switch (numbucket)
-                {
-                    case 1:
-                        box1 = box1 - pickbox;
-                        break;
-                    case 2:
-                        box2 = box2 - pickbox;
-                        break;
-                    case 3:
-                        box3 = box3 - pickbox;
-                        break;
-                    default:
-                        Console.WriteLine("Not found");
-                        break;
-                }
-                if (box1 == 0 & box2 == 0 & box3 == 0)
-                {
-                    Console.WriteLine("The Winner is A");
-                    gameStart = false;
-                }  
+                Console.WriteLine("{0},{1}", x, y);
+                x += dx;
+                y += dy;
+                i++;
             }
             Console.ReadLine();
-		}
+ 
+        }
     }
 }
