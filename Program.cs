27d834e5ecea3d8e0ef37ac20c007f8fc93b310b
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-04-29 07:48:46 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,88 +1,38 @@
 ﻿using System;
 
-namespace DNA_Replication
+namespace Pascal
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string halfDNA, repDNA;
-            char ans, go;
-            bool again = true;
-
-            while (again == true)
+            int rows = 0, val = 1, i, j;
+            bool isValid = false;
+            while (isValid == false)
             {
-                bool going = true, correct = false;
-                halfDNA = Convert.ToString(Console.ReadLine());
-                if (IsvalidSequence(halfDNA))
+                rows = Convert.ToInt32(Console.ReadLine());
+                if (rows < 0)
                 {
-                    Console.WriteLine("Current half DNA sequence : " + halfDNA);
-                    while (going == true)
-                    {
-                        Console.Write("Do you want to replicate it? (Y/N): ");
-                        ans = Convert.ToChar(Console.ReadLine());
-                        if (ans == 'Y')
-                        {
-                            going = false;
-                            repDNA = ReplicateSequence(halfDNA);
-                            Console.WriteLine("Replicated half DNA sequence: " + repDNA);
-                        }
-                        else if (ans == 'N')
-                        {
-                            going = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please input Y or N.");
-                        }
-                    }
+                    Console.WriteLine("Invalid Pascal's triangle row number.");
                 }
                 else
                 {
-                    Console.WriteLine("That half DNA sequence is invalid.");
+                    isValid = true;
                 }
-                while (correct == false)
+            }
+            Console.WriteLine();
+            for (i = 0; i <= rows; i++)
+            {
+                for (j = 0; j <= i; j++)
                 {
-                    Console.Write("Do you want to process another sequence? (Y/N): ");
-                    go = Convert.ToChar(Console.ReadLine());
-                    if (go == 'Y')
-                    {
-                        again = true;
-                        correct = true;
-                    }
-                    else if (go == 'N')
-                    {
-                        again = false;
-                        correct = true;
-                    }
+                    if (j == 0 || i == 0)
+                        val = 1;
                     else
-                    {
-                        Console.WriteLine("Please input Y or N.");
-                    }
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + " ");
                 }
+                Console.WriteLine();
             }
-        }
-
-        static bool IsvalidSequence(string halfDNASequence)
-        {
-            foreach(char nucleotide in halfDNASequence)
-            {
-                if (!"ATCG".Contains(nucleotide))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        static string ReplicateSequence(string halfDNASequence)
-        {
-            string result = "";
-            foreach (char nucleotide in halfDNASequence)
-            {
-                result += "TAGC"["ATCG".IndexOf(nucleotide)];
-            }
-            return result;
         }
     }
 }
