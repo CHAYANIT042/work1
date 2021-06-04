464f168f32f54c0bb63329e9ee47f337b90c17a6
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-06-04 23:55:52 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,45 @@
 ﻿using System;
 
-namespace homeworkNo5
+namespace SortNumbers
 {
     class Program
     {
-        static string InputPath = "c:\\input.txt";
-        static string ConvolutionPath = "c:\\convolution.txt";
-        static string OutputPath = "Output.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Input your file input");
-            InputPath = Console.ReadLine();
+            int[] numbers;
+            int i, j, amount, num;
 
-            Console.WriteLine("Input your file convolution");
-            ConvolutionPath = Console.ReadLine();
+            Console.Write("Enter amount of numbers: ");
+            amount = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input your file output");
-            OutputPath = Console.ReadLine();
-            double[,] image = ReadImageDataFromFile(InputPath);
-            double[,] image_resize = new double[image.GetLength(1) + 3, image.GetLength(0) + 3];
-            double[,] temp_output = new double[image.GetLength(0) + 1, image.GetLength(0) + 1];
-
-            for (int i = 1; i < image.GetLength(1) + 1; i++)
+            numbers = new int[amount];
+            for(i = 0; i < numbers.Length; i++)
             {
-                for (int j = 1; j < image.GetLength(0) + 1; j++)
-                {
-                    image_resize[i, j] = image[i - 1, j - 1];
-                }
+                Console.Write("Enter number #{0}: ", i+1);
+                num = Convert.ToInt32(Console.ReadLine());
+                
+                numbers[i] = num;
             }
-            display(image_resize);
-            image_resize[0, image_resize.GetLength(0) - 2] = image[image.GetLength(1) - 1, 0];
-            image_resize[0, 0] = image[image.GetLength(1) - 1, image.GetLength(0) - 1];
-            image_resize[image_resize.GetLength(1) - 2, image_resize.GetLength(0) - 2] = image[0, 0];
-            image_resize[image_resize.GetLength(1) - 2, 0] = image[0, image.GetLength(1) - 1];
-            for (int i = 1; i < image.GetLength(0) + 1; i++)
+
+            for (i = 0; i < numbers.Length - 1; i++)
             {
-                image_resize[0, i] = image[image.GetLength(0) - 1, i - 1];
-                image_resize[image_resize.GetLength(1) - 2, i] = image[0, i - 1];
-                image_resize[i, 0] = image[i - 1, image.GetLength(1) - 1];
-                image_resize[i, image_resize.GetLength(0) - 2] = image[i - 1, 0];
-            }
-            display(image_resize);
-            double[,] confile = ReadImageDataFromFile(ConvolutionPath);
-            Console.WriteLine(":>" + confile[0, 0]);
-            for (int i = 0; i < image.GetLength(1) + 1; i++)
-            {
-                for (int j = 0; j < image.GetLength(0); j++)
+                for (j = i + 1; j < numbers.Length; j++)
                 {
-                    double sum = 0;
-                    for (int k = 0; k < confile.GetLength(0); k++)
+                    if (numbers[i] > numbers[j])
                     {
-                        for (int n = 0; n < confile.GetLength(1); n++)
-                        {
-                            sum += confile[k, n] * image_resize[i + k, j + n];
-                        }
-                    }
-                    temp_output[i, j] = sum;
-                }
-            }
-            display(temp_output);
-            WriteImageDataToFile(OutputPath, temp_output);
-        }
-        static void display(double[,] list)
-        {
-            Console.WriteLine("=========");
-            for (int i = 0; i < list.GetLength(1) - 1; i++)
-            {
-                for (int j = 0; j < list.GetLength(0) - 1; j++)
-                {
-                    Console.Write(list[i, j] + " ");
-                }
-                Console.WriteLine("");
-            }
-        }
-        static double[,] ReadImageDataFromFile(string imageDataFilePath)
-        {
-            string[] lines = System.IO.File.ReadAllLines(imageDataFilePath);
-            int imageHeight = lines.Length;
-            int imageWidth = lines[0].Split(',').Length;
-            double[,] imageDataArray = new double[imageHeight, imageWidth];
 
-            for (int i = 0; i < imageHeight; i++)
-            {
-                string[] items = lines[i].Split(',');
-                for (int j = 0; j < imageWidth; j++)
-                {
-                    imageDataArray[i, j] = double.Parse(items[j]);
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
                 }
             }
-            return imageDataArray;
-        }
-        static void WriteImageDataToFile(string imageDataFilePath, double[,] imageDataArray)
-        {
-            string imageDataString = "";
-            for (int i = 0; i < imageDataArray.GetLength(0); i++)
+
+            Console.Write("\nNumbers in ascending order: ");
+            for (i = 0; i < numbers.Length; i++)
             {
-                for (int j = 0; j < imageDataArray.GetLength(1) - 1; j++)
-                {
-                    imageDataString += imageDataArray[i, j] + ", ";
-                }
-                imageDataString += imageDataArray[i, imageDataArray.GetLength(1) - 1];
-                imageDataString += "\n";
+                Console.Write(numbers[i]+" ");
             }
-            System.IO.File.WriteAllText(imageDataFilePath, imageDataString);
         }
     }
 }
