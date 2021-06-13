bec9858764166cf507e0354c15d20fb09288b690
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-06-13 15:57:26 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,126 +1,85 @@
 ﻿using System;
 
-namespace ConsoleApp7
+namespace test1
 {
-    enum Difficulty
-    {
-        Easy,
-        Normal,
-        Hard
-    }
-
-    struct Problem
-    {
-        public string Message;
-        public int Answer;
-
-        public Problem(string message, int answer)
-        {
-            Message = message;
-            Answer = answer;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Difficulty level = Difficulty.Easy;
-            Problem[] problems = GenerateRandomProblems(3);
-            int go = 0, ans;
-            bool running = true;
-            long startTime, endTime, totalTime;
-            double score = 0;
+            int[,] nums = new int[3, 3];
+            int row, col;
 
-            while (running)
+            row = 0;
+            col = 0;
+            for(int i = 0; i < 9; i++)
             {
-                Console.WriteLine("Score: {0:R}, Difficulty: {1}", score, level);
-                go = Convert.ToInt32(Console.ReadLine());
-                switch (go)
+                int num = Convert.ToInt32(Console.ReadLine());
+                if(row <= 2)
                 {
-                    case 0:
-                        if ((int)level == 0)
-                            problems = GenerateRandomProblems(3);
-                        else if ((int)level == 1)
-                            problems = GenerateRandomProblems(5);
-                        else if ((int)level == 2)
-                            problems = GenerateRandomProblems(7);
-
-                        double point = 0;
-                        double rightAns = 0, amount = problems.Length;
-                        int levelNum = (int)level;
-
-                        startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
-                        for (int i = 0; i < amount; i++)
-                        {
-                            Console.WriteLine(problems[i].Message);
-                            ans = Convert.ToInt32(Console.ReadLine());
-                            if (ans == problems[i].Answer)
-                                rightAns += 1;
-                        }
-                        endTime = DateTimeOffset.Now.ToUnixTimeSeconds();
-                        totalTime = endTime - startTime;
-                        point = (rightAns / amount) * (25 - Math.Pow(levelNum, 2) / (Math.Max(totalTime, (25 - Math.Pow(levelNum, 2))))) * Math.Pow((2 * levelNum) + 1, 2);
-                        score += point;
-                        break;
-
-                    case 1:
-                        bool inValid = true;
-                        int setting;
-                        while (inValid)
-                        {
-                            setting = Convert.ToInt32(Console.ReadLine());
-                            switch (setting)
-                            {
-                                case 0:
-                                    level = Difficulty.Easy;
-                                    inValid = false;
-                                    break;
-                                case 1:
-                                    level = Difficulty.Normal;
-                                    inValid = false;
-                                    break;
-                                case 2:
-                                    level = Difficulty.Hard;
-                                    inValid = false;
-                                    break;
-                                default:
-                                    Console.WriteLine("Please input 0-2.");
-                                    break;
-                            }
-                        }
-                        break;
-
-                    case 2:
-                        running = false;
-                        break;
-                    default:
-                        Console.WriteLine("Please input 0-2.");
-                        break;
+                    if(col <= 2)
+                    {
+                        nums[row, col] = num;
+                        col++;
+                    }
+                    else
+                    {
+                        col = 0;
+                        row++;
+                        nums[row, col] = num;
+                        col++;
+                    }
                 }
             }
-        }
 
-        static Problem[] GenerateRandomProblems(int numProblem)
-        {
-            Problem[] randomProblems = new Problem[numProblem];
-
-            Random rnd = new Random();
-            int x, y;
-
-            for (int i = 0; i < numProblem; i++)
+            int rowZero = 0, colZero = 0;
+            for(int i = 0; i < 3; i++)
             {
-                x = rnd.Next(50);
-                y = rnd.Next(50);
-                if (rnd.NextDouble() >= 0.5)
-                    randomProblems[i] =
-                        new Problem(String.Format("{0} + {1} = ?", x, y), x + y);
-                else
-                    randomProblems[i] =
-                        new Problem(String.Format("{0} - {1} = ?", x, y), x - y);
+                for(int j = 0; j < 3; j++)
+                {
+                    if(nums[i, j] == 0)
+                    {
+                        rowZero = i;
+                        colZero = j;
+                    }
+                    Console.Write(nums[i, j]+" ");
+                }
+                Console.WriteLine();
             }
 
-            return randomProblems;
+            int numAdd = Convert.ToInt32(Console.ReadLine());
+            bool isValid = false;
+            for(col = 0; col < 3; col++)
+            {
+                if(numAdd == nums[rowZero, col])
+                {
+                    isValid = true;
+                }
+            }
+
+            for (row = 0; row < 3; row++)
+            {
+                if (numAdd == nums[colZero, row])
+                {
+                    isValid = true;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("The number is avalible.");
+            }
+            else
+            {
+                nums[rowZero, colZero] = numAdd;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(nums[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
