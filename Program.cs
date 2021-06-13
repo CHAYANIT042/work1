42578ac27f3af61763cd3f1e6875e3153b20c3e5
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-06-13 16:01:37 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,98 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace test2
+namespace test3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i, j;
-            int count = Convert.ToInt32(Console.ReadLine());
-            int[] arr1 = new int[count];
-            int[] arr2 = new int[count];
-            int[] arr3 = new int[count];
-            int[] arr4 = new int[count];
+            bool isEnd = false, isCorrect = false;
+            List<double> arr = new List<double>();
+            int i = 0, count = 0; ;
+            double number;
 
-            for (i = 0; i < arr1.Length; i++)
+            while(isEnd == false)
             {
-                arr1[i] = Convert.ToInt32(Console.ReadLine());
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out number)){
+                    arr.Add(Convert.ToDouble(input));
+                    i++;
+                    count += 1;
+                }
+                else if(input == "End")
+                {
+                    isEnd = true;
+                }
             }
 
-            arr2 = arr1;
-            arr3 = arr1;
-            arr4 = arr1;
-
-            for (i = 0; i < arr1.Length; i++)
+            while(isCorrect == false)
             {
-                for (j = i; j < arr1.Length; j++)
+                string input = Console.ReadLine();
+                if(input == "FindMax")
                 {
-                    if (arr1[i] > arr1[j])
+                    double max = 0;
+                    for(int j = 0; j < count; j++)
                     {
-                        int temp = arr1[i];
-                        arr1[i] = arr1[j];
-                        arr1[j] = temp;
+                        if(arr[j] > max)
+                        {
+                            max = arr[j];
+                        }
                     }
+                    Console.WriteLine(max);
+                    isCorrect = true;
                 }
-            }
-            Console.Write("Compare's sort: ");
-            for (i = 0; i < arr1.Length; i++)
-            {
-                Console.Write(arr1[i] + " ");
-            }
-            Console.WriteLine();
-
-            Array.Sort(arr2);
-            Console.Write("Com's sort: ");
-            for (i = 0; i < arr2.Length; i++)
-            {
-                Console.Write(arr2[i] + " ");
-            }
-            Console.WriteLine();
-
-            for (i = 0; i < arr3.Length; i++)
-            {
-                int temp = arr3[i];
-                int currentIndex = i;
-
-                while (currentIndex > 0 && arr3[currentIndex - 1] > temp)
+                else if(input == "FindMin")
                 {
-                    arr3[currentIndex] = arr3[currentIndex - 1];
-                    currentIndex--;
-                }
-
-                arr3[currentIndex] = temp;
-            }
-            Console.Write("May's sort: ");
-            for (i = 0; i < arr3.Length; i++)
-            {
-                Console.Write(arr3[i] + " ");
-            }
-            Console.WriteLine();
-
-            for (i = 0; i < arr4.Length; i++)
-            {
-                int min = i;
-                for (j = i + 1; j < arr4.Length; j++)
-                {
-                    if (arr4[min] > arr4[j])
+                    double min = arr[0];
+                    for (int j = 0; j < count; j++)
                     {
-                        min = j;
+                        if (arr[j] < min)
+                        {
+                            min = arr[j];
+                        }
                     }
+                    Console.WriteLine(min);
+                    isCorrect = true;
                 }
-
-                if (min != i)
+                else if (input == "FindMean")
                 {
-                    int lowerValue = arr4[min];
-                    arr4[min] = arr4[i];
-                    arr4[i] = lowerValue;
+                    double sum = 0, avr;
+                    for (int j = 0; j < count; j++)
+                    {
+                        sum += arr[j];
+                    }
+                    avr = sum / count;
+                    Console.WriteLine(avr);
+                    isCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Mode.");
                 }
             }
-            Console.Write("Sea's sort: ");
-            for (i = 0; i < arr4.Length; i++)
-            {
-                Console.Write(arr4[i] + " ");
-            }
-            Console.WriteLine();
         }
     }
 }
