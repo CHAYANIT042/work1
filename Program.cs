329c64238f4813da7c7499f23f383168b1b571e0
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-06-13 16:04:39 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,183 +1,64 @@
 ﻿using System;
 
-namespace test4
+namespace test5
 {
     class Program
     {
-        enum Status
-        {
-            Low,
-            Normal,
-            High,
-            Extreme
-        }
-
-        struct Town
+        struct Item
         {
             public string Name;
-            public int[] TownContacts;
-            public Status stat;
+            public string Type;
 
-            public Town(string name, int[] contact)
+            public Item(string name, string type)
             {
                 Name = name;
-                TownContacts = contact;
-                stat = Status.Low;
+                Type = type;
             }
         }
 
         static void Main(string[] args)
         {
             int count = Convert.ToInt32(Console.ReadLine());
-            Town[] towns = new Town[count];
-            for (int i = 0; i < count; i++)
+            Item[] items = new Item[count];
+            for (int i = 0; i < items.Length; i++)
             {
                 string name = Console.ReadLine();
-                int contactCount = Convert.ToInt32(Console.ReadLine());
-                int[] contacts = new int[contactCount];
-                for (int j = 0; j < contactCount; j++)
-                {
-                    bool isCorrect = false;
-                    while (isCorrect == false)
-                    {
-                        int townID = Convert.ToInt32(Console.ReadLine());
-                        int num = j;
-                        bool valid = true;
-                        if (j != 0)
-                        {
-                            while (num >= 0)
-                            {
-                                if (townID == contacts[num])
-                                {
-                                    valid = false;
-                                    break;
-                                }
-                                else
-                                {
-                                    num--;
-                                }
-                            }
-                            if(valid == true)
-                            {
-                                if(townID >= count)
-                                {
-                                    valid = false;
-                                }
-                                else if(townID == i)
-                                {
-                                    valid = false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (townID >= count)
-                            {
-                                valid = false;
-                            }
-                            if (townID == i)
-                            {
-                                valid = false;
-                            }
-                        }
+                string type = Console.ReadLine();
 
-                        if (valid == true)
-                        {
-                            contacts[j] = townID;
-                            isCorrect = true;
-                        }
-                        else if(valid == false)
-                        {
-                            Console.WriteLine("Invalid ID.");
-                        }
-                    }
-                }
-                towns[i] = new Town(name, contacts);
+                items[i] = new Item(name, type);
             }
 
-            bool isTrue = false;
-            while(isTrue == false)
+            bool isEnd = false;
+            while (isEnd == false)
             {
-                string situation = Console.ReadLine();
-                if(situation == "Outbreak" || situation == "Vaccinate" || situation == "Lock down"){
-                    int townID = Convert.ToInt32(Console.ReadLine());
-                    if(situation == "Outbreak")
-                    {
-                        if((int)towns[townID].stat <= 1)
-                        {
-                            towns[townID].stat += 2;
-                        }
-                        else if((int)towns[townID].stat == 2)
-                        {
-                            towns[townID].stat += 1;
-                        }
-                        for (int k = 0; k < towns[townID].TownContacts.Length; k++)
-                        {
-                            if((int)towns[towns[townID].TownContacts[k]].stat <= 2)
-                            {
-                                towns[towns[townID].TownContacts[k]].stat += 1;
-                            }
-                        }
-                    }
-                    else if(situation == "Vaccinate")
-                    {
-                        towns[townID].stat = 0;
-                    }
-                    else if(situation == "Lock down")
-                    {
-                        if ((int)towns[townID].stat > 0)
-                        {
-                            towns[townID].stat -= 1;
-                        }
-                        for (int k = 0; k < towns[townID].TownContacts.Length; k++)
-                        {
-                            if ((int)towns[towns[townID].TownContacts[k]].stat > 0)
-                            {
-                                towns[towns[townID].TownContacts[k]].stat -= 1;
-                            }
-                        }
-                    }
-                    for (int i = 0; i < count; i++)
-                    {
-                        Console.WriteLine("{0} {1} {2}", i, towns[i].Name, (int)towns[i].stat);
-                    }
-                }
-                else if(situation == "Spread")
+                string search = Console.ReadLine();
+                if(search == "ShowAll")
                 {
-                    bool canFind = false;
-                    for (int j = 0; j < count; j++)
+                    for (int i = 0; i < items.Length; i++)
                     {
-                        for (int k = 0; k < towns[j].TownContacts.Length; k++)
-                        {
-                            if ((int)towns[towns[j].TownContacts[k]].stat > (int)towns[j].stat)
-                            {
-                                canFind = true;
-                                break;
-                            }
-                        }
-                        if(canFind == true)
-                        {
-                            for (int l = 0; l < count; l++)
-                            {
-                                if ((int)towns[l].stat <= 2)
-                                {
-                                    towns[l].stat += 1;
-                                }
-                            }
-                        }
+                        Console.WriteLine(items[i].Name);
+                        Console.WriteLine(items[i].Type);
+                        Console.WriteLine();
                     }
-                    for (int i = 0; i < count; i++)
-                    {
-                        Console.WriteLine("{0} {1} {2}", i, towns[i].Name, (int)towns[i].stat);
-                    }
-                }
-                else if (situation == "Exit")
-                {
-                    isTrue = true;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid");
+                    bool isFound = false;
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        if (search == items[i].Type)
+                        {
+                            Console.WriteLine(items[i].Name);
+                            Console.WriteLine();
+                            isFound = true;
+                        }
+                    }
+
+                    if(isFound == false)
+                    {
+                        Console.WriteLine("End");
+                        isEnd = true;
+                    }
                 }
             }
         }
