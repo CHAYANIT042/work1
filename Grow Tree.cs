f34733df0bdf2dcd31809dc93eccd9d46e2f85d3
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-09-18 10:36:45 +0700

    Add files via upload

---
+++ Grow Tree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace GrowTree
+{
+    class Rose
+    {
+        static void Main(string[] args)
+        {
+            int Rose;
+            int.TryParse(Console.ReadLine(), out Rose);
+            Console.WriteLine("Input total rose : {0}",Rose);
+            int Sunflower;
+            int.TryParse(Console.ReadLine(), out Sunflower);
+            Console.WriteLine("Input total sunflower : {0}", Sunflower);
+
+        }
+    }
+
+
+
+    class List
+    {
+        private int ID;
+       
+        public List(string size, string color, float price)
+        {
+            this.ID = ID;
+            
+        }
+        public void SetID(int ID)
+        {
+            this.ID = ID;
+        }
+
+       
+
+
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Rose Information");
+            Console.WriteLine("Rose Name");
+            Console.WriteLine("Rose Name");
+            
+           
+            Console.WriteLine("Sunflower Name");
+            Console.WriteLine("Rose Name");
+            Console.WriteLine("Rose Name");
+
+        }
+
+    }
+}
